ff095a805385a4647f7393efb3e01e871b4afba0
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2025-05-15 19:11:08 -0600

    w04 project

---
+++ src/js/productList.mjs
@@ -0,0 +1,30 @@
+import { getData } from "./productData.mjs";
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+  return `<li class="product-card">
+    <a href="product_pages/index.html?product=${product.Id}">
+    <img
+      src="${product.Image}"
+      alt="Image of ${product.Name}"
+    />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.NameWithoutBrand}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p></a>
+  </li>`;
+}
+
+export default async function productList(selector, category) {
+  // get the element we will insert the list into from the selector
+  const el = document.querySelector(selector);
+  // get the list of products
+  let products = await getData(category);
+
+  //only show the 4 tents we need
+  const allowedIds = ["880RR", "985RF", "344YJ", "985PR"];
+  products = products.filter(product => allowedIds.includes(product.Id));
+
+  // render out the product list to the element
+  renderListWithTemplate(productCardTemplate, el, products);
+}
+
